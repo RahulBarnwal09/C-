ea31e35e41990adce9a0dc4d08ef64f343997aa2
RahulBarnwal09 <rbarnwal4056@gmail.com>
2021-03-30 14:23:26 +0530

    Add C++ Files

---
+++ datatype.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Size of int "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"Size of float "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"Size of char "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"Size of bool "<<sizeof(bool)<<" bytes"<<endl;
+}
---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int amt1,amt2,sum;
+    cout<<"Enter amount1: ";
+    cin>>amt1;
+    cout<<"Enter amount2: ";
+    cin>>amt2;
+    sum=amt1+amt2;
+    cout<<"Total amount is: "<<sum<<endl;
+    return 0;
+}
